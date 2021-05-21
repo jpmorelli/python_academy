199f9bca305b47e2ab5ad98e0b724f788e67ae4b
Juan Pablo Morelli <jpmorell@ar.ibm.com>
2021-05-21 11:26:27 -0300

    Add first exercises

---
+++ 1a_hola_mundo.py
@@ -0,0 +1,9 @@
+### a. Imprimiendo en pantalla
+#
+#1. Mostrar el mensaje "Hola, Mundo."
+#
+#2. Ingresar el nombre de un usuario y saludarlo. Ejemplo: Si el usuario se llama Juan, debe mostrar el mensaje "Hola, Juan."
+
+print('Hola Mundo')
+name = input ('Cuales tu nombre ?\n')
+print('Hola,',name,'.')
---
+++ 1b_Operaciones_aritméticas.py
@@ -0,0 +1,14 @@
+### b. Operaciones aritméticas
+#
+#1. Ingresar dos números y mostrar la suma y la diferencia.
+#2. Ingresar tres números y mostrar la suma y el promedio.
+#3. Ingresar el monto de una factura y calcular el IVA (21%).
+#4. Calcular el promedio de las notas que obtiene un alumno al rendir dos parciales. Se debe imprimir el nombre del alumno y la nota final.
+#5. Tres personas invierten dinero para fundar una empresa (no necesariamente en partes iguales). Calcular qué porcentaje invirtió cada una. Se debe imprimir el monto total, además #del nombre de cada inversor y el porcentaje contribuído.
+#6. Una persona quiere invertir su capital en un banco y quiere saber cuánto dinero gana en un mes si el banco paga 2% mensual. Se debe mostrar el monto a 3, 6 y 12 meses.
+#7. Una inmobiliaria paga a sus vendedores un salario base, más una comisión fija por cada venta realizada, más el 5% del valor de esas ventas. Realizar un programa que imprima el #número del vendedor y el salario que le corresponde en un determinado mes. Se leen por teclado el número del vendedor, la cantidad de ventas que realizó y el valor total de las #mismas.
+#8. Un banco necesita para sus cajeros de la sucursal un programa que lea una cantidad de dinero que desea retirar el cliente e imprima a cuántos billetes equivale, considerando que #existen billetes de $1000, $500, $200, $100, $50, $20 y el resto en monedas de $10, $5, $2 y $1. Desarrollar dicho programa de tal forma que se minimice la cantidad de billetes #entregados por el cajero.
+#9. Leer un período en segundos e imprimirlo expresado en días, horas, minutos y segundos. Por ejemplo, 200000 segundos equivalen a 2 días, 7 horas, 33 minutos y 20 segundos.
+
+print('Vamos a hacer algunas operaciones artiméticas...\n')
+print()
